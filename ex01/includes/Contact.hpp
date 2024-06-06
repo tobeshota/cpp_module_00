99c88fbf70d978b2e358971664b8e50c457e200e
toshota <toshota@student.42tokyo.jp>
2024-06-06 20:23:54 +0900

    [ref]

--- ex01/includes/Contact.hpp
+++ ex01/includes/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 13:36:34 by toshota           #+#    #+#             */
-/*   Updated: 2024/06/06 20:07:40 by toshota          ###   ########.fr       */
+/*   Updated: 2024/06/06 20:21:22 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,11 @@
 # define CONTACT_HPP
 
 # include <string>
-
-#define FIRST_NAME_PROMPT "first name:\t"
-#define LAST_NAME_PROMPT "last name:\t"
-#define NICKNAME_PROMPT "nickname:\t"
-#define PHONE_NUMBER_PROMPT "phone number:\t"
-#define DARKEST_SECRET_PROMPT "darkest secret:\t"
+# define FIRST_NAME_PROMPT "first name:\t"
+# define LAST_NAME_PROMPT "last name:\t"
+# define NICKNAME_PROMPT "nickname:\t"
+# define PHONE_NUMBER_PROMPT "phone number:\t"
+# define DARKEST_SECRET_PROMPT "darkest secret:\t"
 
 class Contact
 {
@@ -29,26 +28,21 @@ class Contact
 	std::string m_nickname;
 	std::string m_phone_number;
 	std::string m_darkest_secret;
-	void input_first_name();
-	void input_last_name();
-	void input_nickname();
-	void input_phone_number();
-	void input_darkest_secret();
+	void input_first_name(void);
+	void input_last_name(void);
+	void input_nickname(void);
+	void input_phone_number(void);
+	void input_darkest_secret(void);
 	bool is_str_number(const std::string str) const;
-	bool is_str_only_space(std::string str) const;
+	bool is_str_only_space(const std::string str) const;
+
   public:
 	void set();
-
-	std::string get_first_name() const;
-	std::string get_last_name() const;
-	std::string get_nickname() const;
-	std::string get_phone_number() const;
-	std::string get_darkest_secret() const;
+	std::string get_first_name(void) const;
+	std::string get_last_name(void) const;
+	std::string get_nickname(void) const;
+	std::string get_phone_number(void) const;
+	std::string get_darkest_secret(void) const;
 };
 
-
-/*
-クラスでpublicの変数はほぼ見ない．クラスの外から値を変更しようとするとき，publicを用いる．
-値を見たいときは，publicでget_first_name();を用いて出すようにする．
- */
 #endif
--- ex01/includes/PhoneBook.hpp
+++ ex01/includes/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 13:58:49 by toshota           #+#    #+#             */
-/*   Updated: 2024/06/06 20:14:58 by toshota          ###   ########.fr       */
+/*   Updated: 2024/06/06 20:22:00 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ class PhoneBook
 {
   private:
 	std::array<Contact, MAX_CONTACTS_INDEX> m_contact;
-	int m_old_index; //	格納するcontactは8個である．9以降は最も古いインデックスold_indexのcontactと置き換える
+	int m_old_index;
 	void put_phone_book(void) const;
 	void put_search_target(const int index) const;
 	int	string_to_int(const std::string str) const;
